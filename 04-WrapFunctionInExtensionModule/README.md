72d8940d1b7ad7ace3d8874d86ff3ff860ac8c34
Boris Dalstein <dalboris@gmail.com>
2017-08-15 00:25:11 -0700

    Add 04; same as 03 but compile as shared library and import in python3 as extension module

---
+++ 04-WrapFunctionInExtensionModule/README.md
@@ -0,0 +1,23 @@
+Compile and Run:
+
+```
+cd learning-cpython
+mkdir build-04
+cd build-04
+cmake ../04-*
+make
+python3
+```
+
+Possible output (interactive session):
+
+```
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> from MyModule import square
+>>> x = square(12)
+>>> print(x)
+144.0
+>>> exit()
+```
---
+++ 04-WrapFunctionInExtensionModule/main.cpp
@@ -0,0 +1,51 @@
+#include <Python.h>
+
+// Define C++ function
+static double square(double x)
+{
+    return x*x;
+}
+
+// Wrap C++ function in a Python function
+static PyObject *wrapSquare(PyObject *self, PyObject *args)
+{
+    // Convert Python type to C++ type
+    double x;
+    if (!PyArg_ParseTuple(args, "d", &x))
+        return NULL;
+
+    // Call C++ function
+    double xSquared = square(x);
+
+    // Convert C++ type to Python type
+    return PyFloat_FromDouble(xSquared);
+}
+
+// Define list of Python methods
+static PyMethodDef MyModuleMethods[] = 
+{
+    {"square",                                  // method name
+     wrapSquare,                                // method wrapper
+     METH_VARARGS,                              // method calling convention
+     "Computes the square of a given number."}, // method documentation
+
+    {NULL, NULL, 0, NULL} // sentinel
+};
+
+// Define new Python module made of above methods
+static struct PyModuleDef MyModule = 
+{
+    PyModuleDef_HEAD_INIT,
+    "MyModule",              // module name
+    "Provides cool methods", // module documentation
+    -1,                      // size of per-interpreter state of the module,
+                             // or -1 if the module keeps state in global variables.
+    MyModuleMethods          // module methods
+};
+
+// Define module's initialization function.
+// The name of this function must be PyInit_<modulename>.
+PyMODINIT_FUNC PyInit_MyModule(void)
+{
+    return PyModule_Create(&MyModule);
+}
